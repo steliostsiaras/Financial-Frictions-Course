3cf4c81adc1ad1dfab621cb01a38e570760a28e8
steliostsiaras <stelios.tsiarass@gmail.com>
2021-04-07 13:04:46 +0300

    detele exercise solution

--- Dynare/exercise/code/RBC1_inv_adj_set_auxiliary_variables.m
+++
@@ -1,7 +0,0 @@
-function y = RBC1_inv_adj_set_auxiliary_variables(y, x, params)
-%
-% Status : Computes static model for Dynare
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
