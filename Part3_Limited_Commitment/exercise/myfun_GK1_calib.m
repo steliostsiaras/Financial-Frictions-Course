62baadf3c1b58f36b860b899f198970a8c9b30fb
steliostsiaras <stelios.tsiarass@gmail.com>
2021-05-06 15:03:02 +0300

    delete

--- Part3_Limited_Commitment/exercise/myfun_GK1_calib.m
+++
@@ -1,5 +0,0 @@
-
-function F = myfun_GK1_calib(c,sigmab,ksi,betta,phi_mom)
-F(1)=phi_mom - (1-sigmab/betta)/(c(1)*(sigmab+ksi)-sigmab/betta); %Equation 1
-F(2)=phi_mom - ((1-sigmab)+sigmab*phi_mom*c(2))/(c(2)-((1-sigmab)+sigmab*phi_mom*c(2))*(betta*c(1)-1));%Equation 2
-end
