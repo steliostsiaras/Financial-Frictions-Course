19187e3ff134a6b5f9ce1ace16279a982fb7e969
steliostsiaras <stelios.tsiarass@gmail.com>
2021-05-13 14:36:24 +0300

    delete

--- Part4_Monetary_Macropru_Policy/exercise/code/ccyb/+GK_ccyb/dynamic_set_auxiliary_series.m
+++
@@ -1,9 +0,0 @@
-function ds = dynamic_set_auxiliary_series(ds, params)
-%
-% Status : Computes Auxiliary variables of the dynamic model and returns a dseries
-%
-% Warning : this file is generated automatically by Dynare
-%           from model file (.mod)
-
-ds.AUX_ENDO_LAG_0_1=ds.Y(-1);
-ds.AUX_ENDO_LAG_0_2=ds.AUX_ENDO_LAG_0_1(-1);
